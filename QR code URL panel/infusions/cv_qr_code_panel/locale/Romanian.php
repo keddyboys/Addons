2ff9a5e27afc76897f14289cf4ee089695a40205
keddyboys <keddyboys@gmail.com>
2017-05-09 07:37:14 +0300

    Romanian translation for panels.

---
+++ QR code URL panel/infusions/cv_qr_code_panel/locale/Romanian.php
@@ -0,0 +1,27 @@
+<?php
+$locale['cvqr_title'] = "Cod QR";
+$locale['cvqr_descr'] = "Panou care arat&#259; un cod QR cu leg&#259;tur&#259; pentru pagina curent&#259;";
+
+$locale['cvqr_1000'] = "Op&#355;iuni cod-QR";
+$locale['cvqr_1001'] = "Po&#355;i modifica aceste valori pentru o afi&#351;are mai bun&#259; a codului QR pe paginile saitulul t&#259;u";
+$locale['cvqr_1002'] = "Culori";
+$locale['cvqr_1003'] = "M&#259;rimi";
+$locale['cvqr_1004'] = "Fiabilitate";
+
+$locale['cvqr_1011'] = "Imagine";
+$locale['cvqr_1012'] = "Fundal";
+$locale['cvqr_1016'] = "M&#259;rime p&#259;trat (&#238;n pixeli)";
+$locale['cvqr_1017'] = "Fundal (&#238;n p&#259;trat)";//padding
+$locale['cvqr_1021'] = "Nivel corec&#355;ie";
+$locale['cvqr_1022'] = "Mic&#259;";
+$locale['cvqr_1023'] = "Medie";
+$locale['cvqr_1024'] = "Calitate";
+$locale['cvqr_1025'] = "Mare";
+
+// Save Settings
+$locale['cvqr_1100'] = "Salveaz&#259; set&#259;rile";
+
+// Settings Error
+$locale['cvqr_1101'] = "Set&#259;ri actualizare cu succes";
+$locale['cvqr_1102'] = "A ap&#259;rut o eroare a bazei de date: set&#259;rile nu au fost actualizate!";
+?>
---
+++ Users birthday panel/infusions/cv_birthdays_today_panel/locale/Romanian.php
@@ -0,0 +1,4 @@
+<?php
+$locale['btp_001'] = "Zile de na&#351;tere ast&#259;zi";
+$locale['btp_002'] = "Nu exist&#259; zile de na&#351;tere ast&#259;zi...";
+?>
